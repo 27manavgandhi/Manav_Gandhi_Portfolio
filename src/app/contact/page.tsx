1515a8b818ff8be9ddb8d8ff7a727104b3974a66
27manavgandhi <27manavgandhi@gmail.com>
2024-06-05 13:23:55 +0530

    Updating contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,9 +8,9 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: "Contact | John Doe",
+  title: "Contact | Manav Gandhi",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Manav Gandhi is a developer, writer and speaker. He is a digital nomad and loves to travel and explore",
 };
 
 export default function Projects() {
