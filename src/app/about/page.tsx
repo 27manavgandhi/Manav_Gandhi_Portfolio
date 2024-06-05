dc5fcc67077c34a980bafcc91aa27b36cfa5f07d
27manavgandhi <27manavgandhi@gmail.com>
2024-06-05 13:21:52 +0530

    Updating page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,9 +10,9 @@ import { motion } from "framer-motion";
 import About from "@/components/About";
 
 export const metadata: Metadata = {
-  title: "About | John Doe",
+  title: "About | Manav Gandhi",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Manav Gandhi is a developer, writer and speaker. He is a digital nomad and loves to travel and explore",
 };
 
 export default function AboutPage() {
