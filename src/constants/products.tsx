9235414555d3d593c8a2a19af58cf0059b8a7617
27manavgandhi <27manavgandhi@gmail.com>
2024-06-09 11:46:49 +0530

    Updating src/constants/products.tsx

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,134 +1,120 @@
 export const products = [
   {
-    href: "https://aceternity.com",
-    title: "Aceternity",
+    href: "https://taskflow-v1.vercel.app/",
+    title: "Taskflow",
     description:
-      "A design and development studio that focuses on building quality apps.",
-    thumbnail: "/images/sidefolio-aceternity.png",
+      "Taskflow is a versatile productivity app that integrates various functionalities within a unified platform, catering to individual, team, and business workflows.",
+    thumbnail: "/images/manavgandhi-taskflow (1).png",
     images: [
-      "/images/sidefolio-aceternity.png",
-      "/images/sidefolio-aceternity-2.png",
+      "/images/manavgandhi-taskflow (1).png",
+      "/images/manavgandhi-taskflow (2).png",
+      "/images/manavgandhi-taskflow (3).png",
+      "/images/manavgandhi-taskflow (4).png",
+      "/images/manavgandhi-taskflow (5).png",
     ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "aceternity",
+    stack: ["Nextjs", "Typescript", "React", "Edge store", "Clerk", "Tailwindcss", "Convex"],
+    slug: "taskflow",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        The technology stack behind TaskFlow is cutting-edge, leveraging the power of Next.js 14, TypeScript, and React for a dynamic and responsive user interface. Next.js 14 offers server-side rendering and static site generation, ensuring fast load times and optimal performance. TypeScript enhances the codebase with strong typing, reducing errors and improving maintainability. React, a popular JavaScript library, powers the user interface, providing a flexible and efficient way to build interactive components. Clerk handles authentication and user management, ensuring secure and seamless access for all users. Convex deployment simplifies the deployment process, while Vercel hosts the application, providing a reliable and scalable infrastructure.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+        TaskFlow's integration of these advanced technologies results in a powerful productivity tool that caters to diverse user needs. The app's design focuses on usability and efficiency, with features that allow for intuitive task creation, assignment, and tracking. Collaboration is enhanced through shared workspaces and real-time updates, ensuring everyone stays informed and aligned. With its comprehensive feature set and robust technological foundation, TaskFlow stands out as a premier solution for managing workflows in any setting.
         </p>{" "}
       </div>
     ),
   },
   {
-    href: "https://algochurn.com",
-    title: "Algochurn",
+    href: "https://board-wex.vercel.app/",
+    title: "Board Wex",
     description:
-      "Practice for technical interviews with hands on coding challenges.",
-    thumbnail: "/images/sidefolio-algochurn.png",
+      " A collaborative, real-time whiteboard. Packed with features such as real-time database, whiteboard from scratch with ability to add shapes like Rectangles and Ellipses, Sticky notes and Pencil drawing. Using the newest technologies such as Next.js. 14, Clerk, Convex and LiveBlocks.",
+    thumbnail: "/images/manavgandhi-boardwex (1).png",
     images: [
-      "/images/sidefolio-algochurn.png",
-      "/images/sidefolio-algochurn-2.png",
+      "/images/manavgandhi-boardwex (1).png",
+      "/images/manavgandhi-boardwex (2).png",
+      "/images/manavgandhi-boardwex (3).png",
+      "/images/manavgandhi-boardwex (4).png",
+      "/images/manavgandhi-boardwex (5).png",
+      "/images/manavgandhi-boardwex (6).png",
+      "/images/manavgandhi-boardwex (7).png",
+      "/images/manavgandhi-boardwex (8).png",
+      "/images/manavgandhi-boardwex (9).png",
+      "/images/manavgandhi-boardwex (10).png",
+      "/images/manavgandhi-boardwex (11).png",
+      
     ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "algochurn",
+    stack: ["Nextjs", "Tailwindcss", "ShadcnUI", "Typescript", "React", "Convex", "Clerk", "Liveblocks"],
+    slug: "boardwex",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        Board Wex stands out with its robust feature set designed to enhance productivity and creativity. The coloring system allows users to personalize their creations with a variety of hues, while the undo and redo functionality ensures that no idea is ever truly lost, enabling continuous refinement of work. Keyboard shortcuts further streamline the workflow, making it easy for users to access tools and features without breaking their creative flow. Real-time collaboration is at the heart of Board Wex, allowing multiple users to work simultaneously on the same board, seeing each other's changes instantly, and fostering a truly interactive and dynamic environment.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+        Security and usability are paramount in Board Wex. With real-time database integration, every change is saved instantly, ensuring that no progress is lost. The platform's robust authentication system, powered by Clerk, supports organizations and invites, ensuring that only authorized users have access to sensitive workspaces. Additionally, the favoriting functionality allows users to mark and quickly access their most important boards. Styled with TailwindCSS and ShadcnUI, Board Wex combines aesthetic appeal with functionality, making it not just a tool, but a delightful experience for users. Whether for brainstorming sessions, project planning, or creative collaboration, Board Wex is the ultimate digital whiteboard for the modern workspace.
+        </p>{" "}
+      </div>
+    ),
+  },
+
+  {
+    href: "https://Automate360.vercel.app/",
+    title: "Automate360",
+    description:
+      "Automate360 is a powerful SaaS Automation Builder designed to streamline business processes. Built from scratch without integration libraries, it offers a seamless, customizable experience for creating automation flows",
+    thumbnail: "/images/manavgandhi-automate360 (1).png",
+    images: [
+      "/images/manavgandhi-automate360 (1).png",
+      "/images/manavgandhi-automate360 (1).png",
+    ],
+    stack: ["Nextjs", "Typescript", "React", "Neon", "Clerk", "Tailwindcss", "UploadCare"],
+    slug: "automate360",
+    content: (
+      <div>
+        <p>
+        Automate360 include user authentication and management with Clerk Authentication for secure access and Neon Tech for robust performance. Integrations with Google Drive, Slack, Discord, and Notion enable comprehensive connectivity. Payment management is streamlined through Stripe Integration, handling month-to-month subscriptions and payments based on credits consumed. The user interface is designed for ease of use, with a beautiful redesigned node interface, drag-and-drop node builder, mini map for navigation, infinite canvas for complex workflows, and an expandable sidebar. The Aceternity UI provides a sleek, modern look with light and dark modes, complemented by a functioning, attractive landing page.{" "}
+        </p>
+        <p>
+        Automate360 supports deployment and hosting with tools like Ngrok for secure tunnels, Next.js 14 for optimized web applications, and Bun for efficient server-side operations. Users can connect services, create automations using the drag-and-drop builder, monitor changes, and send notifications via Slack and Discord. With customizable features and a user-friendly interface, Automate360 is the ideal platform for businesses aiming to enhance productivity through seamless integration and automation. Start building your automation flows today to experience increased efficiency and streamlined operations.
+
+
+
+
+
+
+
         </p>{" "}
       </div>
     ),
   },
   {
-    href: "https://gomoonbeam.com",
-    title: "Moonbeam",
+    href: "https://objectvision.vercel.app/",
+    title: "Object Vision",
     description:
-      "Never write from scratch again with Moonbeam, your AI first writing tool",
-    thumbnail: "/images/sidefolio-moonbeam.png",
+
+    "A Object Detection Web App using the latest technologies: Next.js 14, React.js, TypeScript, Tailwind CSS, tensorflow for object detection, shadcn/ui for UI components.",
+
+
+    thumbnail: "/images/manavgandhi-boardwex (1).png",
     images: [
-      "/images/sidefolio-moonbeam.png",
-      "/images/sidefolio-moonbeam-2.png",
+      "/images/manavgandhi-boardwex (1).png",
+      "/images/manavgandhi-boardwex (2).png",
+      
     ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "moonbeam",
+    stack: ["Nextjs", "Tailwindcss", "ShadcnUI", "Typescript", "React", "Tensorflow"],
+    slug: "objectvision",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        Incorporating TypeScript into the development process adds a layer of type safety, which is crucial in reducing bugs and enhancing the maintainability of the codebase. This ensures that the application remains robust and scalable as it evolves. The use of Tailwind CSS further elevates the application by providing a highly customizable styling framework that is both efficient and easy to work with. This combination of technologies ensures that ObjectVision is not only functionally powerful but also visually appealing and consistent in its design language.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+        The integration of TensorFlow into ObjectVision is particularly noteworthy as it brings state-of-the-art machine learning capabilities to the forefront of the application. TensorFlow's powerful models drive the core object detection functionality, enabling accurate and efficient identification of objects within images. Additionally, the use of shadcn/ui ensures that the UI components are polished, reusable, and maintain a consistent look and feel throughout the application. This cohesive approach to design and functionality underscores ObjectVision's commitment to providing a cutting-edge, reliable, and aesthetically pleasing solution for object detection.
         </p>{" "}
       </div>
     ),
   },
-  {
-    href: "https://tailwindmasterkit.com",
-    title: "Tailwind Master Kit",
-    description:
-      "A beautiful and comprehensive Tailwind CSS components library for building modern websites and applications.",
-    thumbnail: "/images/sidefolio-tailwindmasterkit.png",
-    images: [
-      "/images/sidefolio-tailwindmasterkit.png",
-      "/images/sidefolio-tailwindmasterkit-2.png",
-    ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "tailwindmasterkit",
-    content: (
-      <div>
-        <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
-        </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
-      </div>
-    ),
-  },
+ 
 ];
